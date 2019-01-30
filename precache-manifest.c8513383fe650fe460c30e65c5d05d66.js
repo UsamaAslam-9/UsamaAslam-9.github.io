f0231adb83ec431d300da415a2ea3bd82b619cc3
Usama Aslam <usamaaslam.code@gmail.com>
2019-01-31 00:27:02 +0500

    database added

---
+++ precache-manifest.c8513383fe650fe460c30e65c5d05d66.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "97721c724ae9d125e9e2",
+    "url": "/static/css/main.6f3c0ee5.chunk.css"
+  },
+  {
+    "revision": "97721c724ae9d125e9e2",
+    "url": "/static/js/main.97721c72.chunk.js"
+  },
+  {
+    "revision": "336fb78a82bec4c1674b",
+    "url": "/static/js/1.336fb78a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ddc736b32fc234306033cbd9e3e9c361",
+    "url": "/static/media/Loader.ddc736b3.svg"
+  },
+  {
+    "revision": "dcdaefb9a2b35458c36a6acfa321a6c2",
+    "url": "/static/media/Close.dcdaefb9.svg"
+  },
+  {
+    "revision": "a1a08994609924646dedf902eef33bd7",
+    "url": "/static/media/Background2.a1a08994.jpg"
+  },
+  {
+    "revision": "b5ef78cf2e3807c3e225274eedbc455b",
+    "url": "/static/media/Menu.b5ef78cf.svg"
+  },
+  {
+    "revision": "8b3d89b11b87f2a5987f1cb9259eff0d",
+    "url": "/index.html"
+  }
+];
